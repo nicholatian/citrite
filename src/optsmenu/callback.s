968ef5a0b0f3c27a795fc65f1e03d500091af4ff
Alexander Nicholi <alex@pokecommunity.com>
2016-06-02 00:49:48 -0400

    Add callback and vblanker for options menu

---
+++ src/optsmenu/callback.s
@@ -0,0 +1,57 @@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@                                                                           @@
+@@   ########## ####### ######### ##########  ####### ######### ##########   @@
+@@   ###    ###   ###   #  ###  # ###     ###   ###   #  ###  # ###    ###   @@
+@@   ###          ###      ###    ###     ###   ###      ###    ###          @@
+@@   ###          ###      ###    #########     ###      ###    #######      @@
+@@   ###          ###      ###    ###   ###     ###      ###    ###          @@
+@@   ###    ###   ###      ###    ###    ###    ###      ###    ###    ###   @@
+@@   ########## #######    ###    ###     ### #######    ###    ##########   @@
+@@                                                                           @@
+@@                          Pokémon Citrite Version                          @@
+@@                              OFFICIAL Source                              @@
+@@                                                                           @@
+@@                  Copyright © 2014-2016 Alexander Nicholi                  @@
+@@                            All rights reserved                            @@
+@@                                                                           @@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+.file "optsmenu/callback.s"
+.ident "AS: (devkitARM release 45) 2.25.1"
+
+@ =========================== S U B R O U T I N E =========================== @
+@
+@ TITLE:       Options menu loader callback
+@ DESCRIPTION: Callback registered with the superstate for the options menu
+@              loader.
+
+.section .text
+.balign 4
+.thumb_func
+.globl optsmenu_load_callback
+
+optsmenu_load_callback:
+    
+    PUSH    {LR}
+    LDR     R1, task_exec
+    BL      .Llinker
+    
+    LDR     R1, objc_exec
+    BL      .Llinker
+    
+    LDR     R1, obj_sync_something
+    BL      .Llinker
+    
+    LDR     R1, fade_and_return_progress_probably
+    BL      .Llinker
+    
+    POP     {R0}
+    BX      R0
+
+.Llinker:
+    
+    BX      R1
+
+@ End of function optsmenu_load_callback
+
+@ -----------------------------------------------------------------------------
---
+++ src/optsmenu/vblanker.s
@@ -0,0 +1,54 @@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@                                                                           @@
+@@   ########## ####### ######### ##########  ####### ######### ##########   @@
+@@   ###    ###   ###   #  ###  # ###     ###   ###   #  ###  # ###    ###   @@
+@@   ###          ###      ###    ###     ###   ###      ###    ###          @@
+@@   ###          ###      ###    #########     ###      ###    #######      @@
+@@   ###          ###      ###    ###   ###     ###      ###    ###          @@
+@@   ###    ###   ###      ###    ###    ###    ###      ###    ###    ###   @@
+@@   ########## #######    ###    ###     ### #######    ###    ##########   @@
+@@                                                                           @@
+@@                          Pokémon Citrite Version                          @@
+@@                              OFFICIAL Source                              @@
+@@                                                                           @@
+@@                  Copyright © 2014-2016 Alexander Nicholi                  @@
+@@                            All rights reserved                            @@
+@@                                                                           @@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+.file "optsmenu/vblanker.s"
+.ident "AS: (devkitARM release 45) 2.25.1"
+
+@ =========================== S U B R O U T I N E =========================== @
+@
+@ TITLE:       Options menu loader V-blank handler
+@ DESCRIPTION: V-blank handler registered with the superstate for the options
+@              menu loader.
+
+.section .text
+.balign 4
+.thumb_func
+.globl optsmenu_vblanker
+
+optsmenu_load_vblanker:
+    
+    PUSH    {LR}
+    LDR     R1, gpu_sprites_upload
+    BL      .Llinker
+    
+    LDR     R1, copy_queue_process
+    BL      .Llinker
+    
+    LDR     R1, gpu_pal_upload
+    BL      .Llinker
+    
+    POP     {R0}
+    BX      R0
+
+.Llinker:
+    
+    BX      R1
+
+@ End of function optsmenu_load_vblanker
+
+@ -----------------------------------------------------------------------------
